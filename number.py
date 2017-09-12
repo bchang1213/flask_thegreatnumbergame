3ed451602e859f644fb04d171ad19d9dfed3bf75
Brian Chang <kyoukamu@gmail.com>
2017-09-12 00:30:23 -0700

    project done

--- number.py
+++ number.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
+import random
 
 app = Flask(__name__)
 app.secret_key = 's@54e236ecr8v0x8972f@#$5vs!$'
 
-@app.route("")
-def initialize():
+# When user loads page
+@app.route("/")
+def index():
+	if not 'answer' in session:
+		session['answer'] = random.randrange(1,101)
+	print session['answer']
 	return	render_template('index.html')
 
+# What happens after the user chooses number
+@app.route("/result", methods =["POST"])
+def result():
+	number = int(request.form['number']) #number is a variable, stored with the user-
+										#-input, and type-cast into integer type.
+	if number > session['answer']:
+		session['message'] = "Too High!"
+
+	elif number < session['answer']:
+		session['message'] = "Too Low.."
+	else:
+		session['message'] = str(session['answer'])+' was the number!'
+	return redirect ('/')
+
+# What happens when the user resets the game upon winning
+@app.route('/reset')
+def reset():
+	session.pop('message') #session is an object.
+	session.pop('answer') #We pop the value paired to-
+	return redirect("/") #-the key of "message" out of the object.
 
 app.run(debug=True)
